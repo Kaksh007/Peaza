59aa76d142dfd314c674a5b53ae070483b8f59c1
Kaksh007 <125899541+Kaksh007@users.noreply.github.com>
2024-10-18 15:37:54 +0530

    all files added

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { persistor, store } from "./redux/store";
+
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
